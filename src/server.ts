2bbaf1a73b191a883ae640fcac8681142075e7e7
AudreyBergamine <audreybergamini@outlook.com>
2024-03-24 12:59:44 -0300

    Atualição

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,12 @@ import departmentsRouter from "./Routes/Rotas";
 import bodyParser from "body-parser";
 
 const app = express();
-app.use(departmentsRouter);
 
 // Configurar o body-parser para analisar solicitações com 'x-www-form-urlencoded'
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(departmentsRouter);
 
 app.listen(3333, () => {console.log("Conectado")})
 
